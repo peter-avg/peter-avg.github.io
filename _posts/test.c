bd6246be68d54b19b0199a459e1f24d37e0fecae
Peter Avgerinos <peteravgerinos@gmail.com>
2026-05-06 17:45:03 +0100

    feature: added script that checks error in rounding modes

--- _posts/test.c
+++ _posts/test.c
@@ -3,69 +3,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 10
-#define DEN 1000
+#define N 10000
+#define DEN 10000
+
+#define F16
+// #define F32
+
+struct Result { float value; float error; };
+
+char* mode_string(int mode) {
+    switch (mode) {
+        case FE_DOWNWARD:
+            return "FE_DOWNWARD";
+        case FE_UPWARD:
+            return "FE_UPWARD";
+        case FE_TOWARDZERO:
+            return "FE_TOWARDZERO";
+        case FE_TONEAREST:
+            return "FE_TONEAREST";
+        default:
+            return "None";
+    }
+}
+
+void result_print(struct Result* res, int mode) {
+};
+
+#ifdef F16
+void result_populate(double res_actual, float* a, float* b, _Float16* c, struct Result* res, int n, int mode) {
+#else
+void result_populate(double res_actual, float* a, float* b, float* c, struct Result* res, int n, int mode) {
+#endif
+    float res_val = 0;
+
+    fesetround(mode);
+
+    for (int i=0; i<n; i++) {
+        c[i] = a[i]*b[i];
+        res_val += c[i]; 
+    }
+
+    float error = fabs(res_actual - res_val);
+
+    res->value = res_val;
+    res->error = error;
+}
+
+void init(struct Result* res, float* a, float* b, double* c, int n, int seed) {
+    srand(seed);
+    for (int i=0; i<n; i++) a[i] = (float)rand() / (float)RAND_MAX;  // NOLINT
+    for (int i=0; i<n; i++) b[i] = (float)rand() / (float)RAND_MAX;  // NOLINT
+
+    double res_val = 0;
+    for (int i=0; i<N; i++) {
+        c[i] = (double)a[i] * (double)b[i];
+        res_val += c[i];
+    }
+
+    res->value = res_val;
+    res->error = 0;
+}
 
 int main(int argc, char* argv[]) {
 
     float* a = (float*)malloc(N * sizeof(float));
     float* b = (float*)malloc(N * sizeof(float));
-
     double* c = (double*)calloc(N,  sizeof(double));
 
-    float* c_zero = (float*)calloc(N,  sizeof(float));
-    float* c_nearest = (float*)calloc(N,  sizeof(float));
-    float* c_up = (float*)calloc(N,  sizeof(float));
-    float* c_down = (float*)calloc(N,  sizeof(float));
+    struct Result* res = (struct Result*)malloc(sizeof(struct Result));
+    init(res, a, b, c, N, 42);
 
-    srand(42);
-    for (int i=0; i<N; i++) a[i] = 1.0f / (float)((rand() % (DEN-1)) + 1);  // NOLINT
-    for (int i=0; i<N; i++) b[i] = 1.0f / (float)((rand() % (DEN-1)) + 1);  // NOLINT
+    int modes[4] = { FE_TOWARDZERO, FE_DOWNWARD, FE_UPWARD, FE_TONEAREST };
 
-    double res = 0;
-    for (int i=0; i<N; i++) {
-        c[i] = (double)a[i] * (double)b[i];
-        res += c[i];
+    for (int i=0; i<4; i++) {
+#ifdef F16
+        _Float16* c_inner = (_Float16*)calloc(N,  sizeof(_Float16));
+#else
+        float* c_inner = (float*)calloc(N,  sizeof(float));
+#endif
+        struct Result* res_inner = (struct Result*)malloc(sizeof(struct Result));
+
+        result_populate(res->value, a, b, c_inner, res_inner, N, modes[i]);
+        printf("Mode: %s, Value: %e, Error: %e\n", mode_string(modes[i]), res_inner->value, res_inner->error);
+
+        free(c_inner); free(res_inner);
     }
-    printf("Actual: %e\n", res);
 
-    float res_zero = 0;
-    fesetround(FE_TOWARDZERO);
-    for (int i=0; i<N; i++) {
-        c_zero[i] = a[i]*b[i];
-        res_zero += c_zero[i]; 
-    }
-    float error_zero = fabs(res - res_zero);
-    printf("Round towards zero error: %e\n", error_zero);
-
-    float res_down = 0;
-    fesetround(FE_DOWNWARD);
-    for (int i=0; i<N; i++) {
-        c_down[i] = a[i]*b[i];
-        res_down += c_down[i]; 
-    }
-    float error_down = fabs(res - res_down);
-    printf("Round downward error: %e\n", error_down);
-
-    float res_up = 0;
-    fesetround(FE_UPWARD);
-    for (int i=0; i<N; i++) {
-        c_up[i] = a[i]*b[i];
-        res_up += c_up[i]; 
-    }
-    float error_up   = fabs(res - res_up);
-    printf("Round upward error: %e\n", error_up);
-
-    float res_nearest = 0;
-    fesetround(FE_TONEAREST);
-    for (int i=0; i<N; i++) {
-        c_nearest[i] = a[i]*b[i];
-        res_nearest += c_nearest[i]; 
-    }
-    float error_nearest = fabs(res - res_nearest);
-    printf("Round to nearest error: %e\n", error_nearest);
-
-    free(a); free(b); free(c); free(c_zero); free(c_nearest); free(c_up); free(c_down);
+    free(a); free(b); free(c); free(res);
 
     return 0;
 }
